bd0295bc65b230c57525b082125be21ff7a9758c
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2019-10-17 17:48:50 -0400

    updated

--- Team Source code folder for collaboration/frontend.py
+++ Team Source code folder for collaboration/frontend.py
@@ -1,93 +1,90 @@
 import sys
 
-class Frontend():
+class Frontend:
 
-    def __init__(self,accountNumber):
+    def __init__(self, accountNumber):
         self.accountNumber = accountNumber
+
+    def getAccount(self):
+        return str(self.accountNumber)
 
     def fileRead(self):
         accountsFileDir = sys.argv[1]
         with open(accountsFileDir) as accountsFile:
            self.accountsList = accountsFile.read().splitlines()
         return self.accountsList
-    '''
-    def __str__(self):
-        return str(self)
-    '''
-    def writeToFile(self,tranCode,toAcctNum,amountCent,fromAcctNum,acctName):
+
+    def writeToFile(self, tranCode, toAcctNum, amountCent, fromAcctNum, acctName):
         if toAcctNum is None:
             toAcctNum = "0000000"
-            #print(toAcctNum)
         if amountCent is None:
             amountCent = "000"
-            #print(amountCent)
         if fromAcctNum is None:
             fromAcctNum = "0000000"
-            #print(fromAcctNum)
         if acctName is None:
             acctName = "***"
-            #print(acctName)
+        #   Writes the Transaction Summary File to the appropriate folder
+        file = open(sys.argv[2], "w+")
+        file.write(tranCode + " " + toAcctNum + " " + amountCent + " " + fromAcctNum + " " + acctName + "\n" + "EOS") # END WITH EOS
 
-        file = open(sys.argv[2],"w")
-        file.write(tranCode + " " + toAcctNum + " " + amountCent + " " + fromAcctNum + " " + acctName) #END WITH EOS
-
-    #if toacct is Null, set to 7 0s
-    #ammount if null, is 000
-    #from acct is Null, 7 0s
-    # if account name is null its ***
-
-    #compares if the given account number is valid or not,checks with the account number present in the file
-    def compare(self,other):
+    #   compares if the given account number is valid or not,checks with the account number present in the file
+    def compare(self, other):
         for i in self.accountsList:
             if i == str(other.accountNumber):
                 return True
         return False
 
 class Withdraw(Frontend):
-    def __init__(self,accountNumber,amount):
+    def __init__(self, accountNumber, amount):
         super().__init__(accountNumber)
         self.amount = int(amount)
-    #Checks if the person has entered the valid number to deposit
+    #   Checks if the person has entered the valid number to deposit
+    def getAmount(self):
+        return str(self.amount/100)
     def checkAmount(self):
-        if self.amount >= 0 and self.amount <= 99999999 :
+        if self.amount >= 0 and self.amount <= 99999999:
             return True
         else:
             print("Invalid amount !")
             return False
 
 class Deposit(Frontend):
-    def __init__(self,accountNumber,amountDeposit):
+    def __init__(self, accountNumber, amountDeposit):
         super().__init__(accountNumber)
         self.amountDeposit = int(amountDeposit)
 
+    def getAmount(self):
+        return str(self.amountDeposit/100)
+
     def checkAmount(self):
-        if self.amountDeposit >= 0 and self.amountDeposit <= 99999999 :
+        if self.amountDeposit >= 0 and self.amountDeposit <= 99999999:
             return True
         else:
             print("Invalid amount !")
             return False
 
 def main():
-    f1 = Frontend(0000000)
+    '''
+    f1 = Frontend(0000000) [FIXED ISSUE, CAN BE DELETED ]
     f1.fileRead() #used later on to check for the valid entered account !
-    userIn = input("enter your choice: ")
+    '''
+    userIn = input("Enter your choice: ")
     userInput = userIn.lower()
     if userInput == "login":
         print("login works")
         pass
-        #login
     elif userInput == "createaccount":
         pass
     elif userInput == "withdraw":
         accountNumber = input("Enter your account Number: ")
         amountWithdraw = input("Enter amount to withdraw in cents: ")
         w1 = Withdraw(accountNumber, amountWithdraw)
-        result1 = f1.compare(w1)
+        w1.fileRead()
+        result1 = w1.compare(w1)
         result2 = w1.checkAmount()
-        if result1 == True and result2 == True:
+        if result1 is True and result2 is True:
             w1.writeToFile("WDR",None,str(amountWithdraw),str(accountNumber),None)
-
-            print("Making a withdraw is possible")
+            print("You withdrew " + "$" + w1.getAmount() + " from " + w1.getAccount())
         else:
             print("valid account number was not entered")
         pass
@@ -95,11 +92,12 @@ def main():
         accountNumber = input("Enter your account Number: ")
         depositAmount = input("Enter amount to deposit: ")
         d1 = Deposit(accountNumber,depositAmount)
-        result1 = f1.compare(d1)
+        d1.fileRead()
+        result1 = d1.compare(d1)
         result2 = d1.checkAmount()
-        if result1 == True and result2 == True:
+        if result1 is True and result2 is True:
             d1.writeToFile("DEP", str(accountNumber), str(depositAmount), None, None)
-            print("Making a deposit to this account is possible")
+            print("You deposited " + "$" + d1.getAmount() + " to " + d1.getAccount())
         else:
             print("invalid entered account number")
         pass
