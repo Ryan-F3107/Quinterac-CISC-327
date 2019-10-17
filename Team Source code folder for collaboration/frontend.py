5864af8daf58acd755b800ff092bb429909c60e8
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2019-10-16 23:06:13 -0400

    Updated at 11pm,Wednesday 16th

--- Team Source code folder for collaboration/frontend.py
+++ Team Source code folder for collaboration/frontend.py
@@ -1,5 +1,4 @@
 import sys
-#import withdraw
 
 class Frontend():
 
@@ -12,18 +11,31 @@ class Frontend():
            self.accountsList = accountsFile.read().splitlines()
         return self.accountsList
     '''
-    def validAccount(accountNum):
-        validAccountList = fileRead()
-        for i in validAccountList:
-            if accountNum == validAccountList[int(i)]:
-                print("It\'s legit")
-                return True
-        return False
-    '''
-    '''
     def __str__(self):
         return str(self)
     '''
+    def writeToFile(self,tranCode,toAcctNum,amountCent,fromAcctNum,acctName):
+        if toAcctNum is None:
+            toAcctNum = "0000000"
+            #print(toAcctNum)
+        if amountCent is None:
+            amountCent = "000"
+            #print(amountCent)
+        if fromAcctNum is None:
+            fromAcctNum = "0000000"
+            #print(fromAcctNum)
+        if acctName is None:
+            acctName = "***"
+            #print(acctName)
+
+        file = open(sys.argv[2],"w")
+        file.write(tranCode + " " + toAcctNum + " " + amountCent + " " + fromAcctNum + " " + acctName) #END WITH EOS
+
+    #if toacct is Null, set to 7 0s
+    #ammount if null, is 000
+    #from acct is Null, 7 0s
+    # if account name is null its ***
+
     #compares if the given account number is valid or not,checks with the account number present in the file
     def compare(self,other):
         for i in self.accountsList:
@@ -34,16 +46,30 @@ class Frontend():
 class Withdraw(Frontend):
     def __init__(self,accountNumber,amount):
         super().__init__(accountNumber)
-        self.amount = amount
+        self.amount = int(amount)
+    #Checks if the person has entered the valid number to deposit
+    def checkAmount(self):
+        if self.amount >= 0 and self.amount <= 99999999 :
+            return True
+        else:
+            print("Invalid amount !")
+            return False
 
 class Deposit(Frontend):
     def __init__(self,accountNumber,amountDeposit):
         super().__init__(accountNumber)
-        self.amountDeposit = amountDeposit
+        self.amountDeposit = int(amountDeposit)
+
+    def checkAmount(self):
+        if self.amountDeposit >= 0 and self.amountDeposit <= 99999999 :
+            return True
+        else:
+            print("Invalid amount !")
+            return False
+
 def main():
     f1 = Frontend(0000000)
-    f1.fileRead()
-    print(f1.fileRead())
+    f1.fileRead() #used later on to check for the valid entered account !
     userIn = input("enter your choice: ")
     userInput = userIn.lower()
     if userInput == "login":
@@ -54,10 +80,13 @@ def main():
         pass
     elif userInput == "withdraw":
         accountNumber = input("Enter your account Number: ")
-        amountWithdraw = input("Enter amount to withdraw: ")
+        amountWithdraw = input("Enter amount to withdraw in cents: ")
         w1 = Withdraw(accountNumber, amountWithdraw)
-        result = f1.compare(w1)
-        if result == True:
+        result1 = f1.compare(w1)
+        result2 = w1.checkAmount()
+        if result1 == True and result2 == True:
+            w1.writeToFile("WDR",None,str(amountWithdraw),str(accountNumber),None)
+
             print("Making a withdraw is possible")
         else:
             print("valid account number was not entered")
@@ -66,8 +95,10 @@ def main():
         accountNumber = input("Enter your account Number: ")
         depositAmount = input("Enter amount to deposit: ")
         d1 = Deposit(accountNumber,depositAmount)
-        result = f1.compare(d1)
-        if result == True:
+        result1 = f1.compare(d1)
+        result2 = d1.checkAmount()
+        if result1 == True and result2 == True:
+            d1.writeToFile("DEP", str(accountNumber), str(depositAmount), None, None)
             print("Making a deposit to this account is possible")
         else:
             print("invalid entered account number")
